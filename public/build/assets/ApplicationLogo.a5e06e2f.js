cc413e0e6e28b8f080803ac3ac5298567a6491c6
Tony Triad <triadsyndicate@gmail.com>
2022-12-04 11:18:42 +0300

    build

---
+++ public/build/assets/ApplicationLogo.a5e06e2f.js
@@ -0,0 +1,1 @@
+import{_ as o}from"./_plugin-vue_export-helper.cdc0426e.js";import{o as s,c}from"./app.5574c2a7.js";const e="/build/assets/vscodeLogo.e25fe0ff.png",t={},r={src:e,class:"rounded-full"};function n(_,a){return s(),c("img",r)}const p=o(t,[["render",n]]);export{p as A};
